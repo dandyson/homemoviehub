555ceb39bb87a48794875b42abcc0a1b6e2dea44
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2023-11-27 15:33:33 +0000

    Fonts added & Basic Hero added to dashboard

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -19,11 +19,9 @@ const showingNavigationDropdown = ref(false);
                     <div class="flex justify-between h-16">
                         <div class="flex">
                             <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
-                                    />
+                            <div class="shrink-0 flex items-center logo-title">
+                                <Link :href="route('dashboard')" class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight font-bebas tracking-wider text-2xl mt-1">
+                                    DYSONFLIX
                                 </Link>
                             </div>
 
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -7,16 +7,35 @@ import { Head } from '@inertiajs/vue3';
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
-        </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+        <section
+            class="relative bg-[url(https://plus.unsplash.com/premium_photo-1675824592772-4d0f0daa067e?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center bg-no-repeat">
+            <div class="absolute inset-0 bg-black/50">
+            </div>
+            <div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+                <div class="max-w-xl text-center sm:text-left max-w-xl text-center sm:text-left px-4 sm:px-0">
+                    <h1 class="text-3xl font-extrabold sm:text-5xl text-white">
+                        West Burton <br>October 2023
+                    </h1>
+
+                    <p class="mt-4 max-w-lg text-center sm:text-xl sm:text-left text-white">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+                        tenetur fuga ducimus numquam ea!
+                    </p>
+
+                    <div class="mt-8 flex flex-wrap gap-4 text-center">
+                        <a href="#"
+                            class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto">
+                            Play
+                        </a>
+
+                        <a href="#"
+                            class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto">
+                            See Info
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     </AuthenticatedLayout>
 </template>
