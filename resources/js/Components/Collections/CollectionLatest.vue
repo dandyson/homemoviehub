7c7d021faec522b7ee197991b5d288c656befb17
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2023-12-04 13:04:27 +0000

    Added latest collection and styled

---
+++ resources/js/Components/Collections/CollectionLatest.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+const media = ref([
+    {
+      text: "Spain October 2022",
+      subtitle: "Family Tip to Alicante in October 2022",
+      image:
+        "https://images.pexels.com/photos/17216084/pexels-photo-17216084/free-photo-of-nourriture-tasse-fruit-panier.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/17216084/pexels-photo-17216084/free-photo-of-nourriture-tasse-fruit-panier.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      text: "title",
+      subtitle: "subtitle",
+      image:
+        "https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+]);
+</script>
+
+<template>
+    <section class="relative bg-white dark:bg-gray-600">
+        <!-- Section -->
+        <div class="absolute inset-0 bg-black/50">
+        </div>
+        <div class="relative mx-auto max-w-screen-xl p-4 py-12 grid md:grid-cols-3 gap-6">
+            <div v-for="(item, index) in media" :key="index" class="relative">
+                <div class="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl relative">
+                    <!-- Card Overlays -->
+                    <div class="absolute inset-0 rounded-xl bg-gradient-to-br from-black to-black opacity-50"></div>
+                    
+                    <div class="absolute inset-0 flex flex-col justify-between text-white px-4 py-6 z-10">
+                        <div class="collection-button-container">
+                            <p class="font-bold text-2xl">{{ item.text }}</p>
+                            <p>{{ item.subtitle }}</p>
+                        </div>
+                        <div class="collection-button-container">
+                            <a href="#" class="rounded bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring active:bg-indigo-500 px-3 py-2 text-sm font-medium me-4">
+                                <font-awesome-icon icon="fa-solid fa-play" /> <span class="ml-1">Play</span>
+                            </a>
+                            <Link :href="route('video-details')" class="rounded bg-white px-3 py-2 text-sm font-medium text-indigo-600 shadow hover:text-indigo-700 focus:outline-none focus:ring active:text-indigo-500 sm:w-auto">
+                                <font-awesome-icon icon="fa-solid fa-circle-info" /> <span class="ml-1">Details</span>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <img
+                        class="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+                        :src="item.image"
+                        alt="/"
+                    />
+                </div>
+            </div>
+        </div>
+        
+    </section>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,15 +1,16 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import CollectionLatest from '@/Components/Collections/CollectionLatest.vue';
+
 </script>
 
 <template>
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-
         <section
-            class="relative bg-[url(https://plus.unsplash.com/premium_photo-1675824592772-4d0f0daa067e?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center bg-no-repeat">
+            class="relative bg-[url(https://plus.unsplash.com/premium_photo-1675824592772-4d0f0daa067e?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center bg-no-repeat h-screen">
             <div class="absolute inset-0 bg-black/50">
             </div>
             <div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
@@ -25,17 +26,19 @@ import { Head } from '@inertiajs/vue3';
 
                     <div class="mt-8 flex flex-wrap gap-4 text-center">
                         <a href="#"
-                            class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto">
+                            class="block w-full rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring active:bg-indigo-500 sm:w-auto">
                             <font-awesome-icon icon="fa-solid fa-play" /> <span class="ml-1">Play</span>
                         </a>
 
                         <a href="#"
-                            class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto">
-                            <font-awesome-icon icon="fa-solid fa-circle-info" /> <span class="ml-1">See Info</span>
+                            class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-indigo-600 shadow hover:text-indigo-700 focus:outline-none focus:ring active:text-indigo-500 sm:w-auto">
+                            <font-awesome-icon icon="fa-solid fa-circle-info" /> <span class="ml-1">Details</span>
                         </a>
                     </div>
                 </div>
             </div>
         </section>
     </AuthenticatedLayout>
+
+    <CollectionLatest></CollectionLatest>
 </template>
