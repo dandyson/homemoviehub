e086d3541ddc97f67e39dfe2a6dc669297f45028
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2023-12-08 15:38:16 +0000

    Changing view for user if they have no videos

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -15,6 +15,7 @@ const latestVideoBgStyle = ref({
   backgroundSize: 'cover',
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
+  height: !latestVideo ? '45rem' : '100vh',
 });
 </script>
 
@@ -22,10 +23,10 @@ const latestVideoBgStyle = ref({
   <Head title="Dashboard" />
 
   <AuthenticatedLayout>
-    <section class="relative lg:h-[45rem]" :style="latestVideoBgStyle">
+    <section class="relative" :style="latestVideoBgStyle">
       <div class="absolute inset-0 bg-black/50"></div>
-      <div class="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 flex justify-center items-center lg:justify-start h-[30rem] lg:h-full lg:px-8">
-        <div v-if="latestVideo" class="max-w-xl text-center sm:text-left max-w-xl text-center sm:text-left px-4 sm:px-0">
+      <div v-if="latestVideo" class="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 flex justify-center items-center lg:justify-start h-[30rem] lg:h-full lg:px-8">
+        <div class="max-w-xl text-center sm:text-left max-w-xl text-center sm:text-left px-4 sm:px-0">
           <h1 class="text-2xl sm:text-3xl md:text-5xl font-extrabold text-white">
             {{ latestVideo?.title }}
           </h1>
@@ -43,14 +44,18 @@ const latestVideoBgStyle = ref({
             </Link>
           </div>
         </div>
-        <div v-else class="max-w-xl text-center sm:text-left max-w-xl text-center sm:text-left px-4 sm:px-0">
+      </div>
+      <div v-else class="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 flex justify-center items-center text-center h-full lg:px-8">
+        <div class="max-w-xl h-full max-w-xl text-center px-4 sm:px-0 flex flex-col items-center justify-center">
           <h1 class="text-2xl sm:text-3xl md:text-5xl font-extrabold text-white">
-            Your latest video will go here
+            You don't have any videos yet.
           </h1>
 
-          <p class="mt-4 max-w-lg text-center sm:text-xl sm:text-left text-white">
-            Please click 'Add Video' in the Navigation to add your first video!
-          </p>
+        <Link :href="route('video.create')" class="relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 mt-8">
+            <span class="relative px-3 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                <font-awesome-icon icon="fa-solid fa-file-video" size="xl" class="me-1" /> + Add Your First Video!
+            </span>
+        </Link>
 
         </div>
       </div>
