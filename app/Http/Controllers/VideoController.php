4974fa381456cf3a548da0873e510e248ca62074
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2023-12-10 15:55:43 +0000

    putting things back to normal

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -110,9 +110,9 @@ class VideoController extends Controller
 
     public function handleCoverImageUpload(Request $request, Video $video = null)
     {
-        // $request->validate([
-        //     'cover_image' => 'required|image|mimes:jpeg,png,jpg|max:10000',
-        // ]);
+        $request->validate([
+            'cover_image' => 'required|image|mimes:jpeg,png,jpg|max:10000',
+        ]);
 
         $path = "cover-images/{$video->id} - {$video->title}";
         $name = $request->file('cover_image')->getClientOriginalName();
@@ -136,7 +136,7 @@ class VideoController extends Controller
             ]);
         } catch (\Exception $e) {
             \Log::error('Error uploading cover image: ' . $e->getMessage());
-            return response()->json(['error' => 'Internal Server Error', 'message' => $e], 500);
+            return response()->json(['error' => 'Internal Server Error'], 500);
         }
     }
 }
