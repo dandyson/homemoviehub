ca6913e034585a7303296755406d370c4968c957
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2024-01-15 17:24:31 +0000

    Added to inertia middleware to include the previous URL (for back buttons and redirects where needed)

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
 
@@ -39,6 +40,13 @@ class HandleInertiaRequests extends Middleware
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
+            'previous' => function () {
+                $previousUrl = URL::previous();
+                $parsedUrl = parse_url($previousUrl);
+    
+                // Check if 'path' exists in the parsed URL and return it, otherwise, return the original URL
+                return isset($parsedUrl['path']) ? $parsedUrl['path'] : $previousUrl;
+            },
         ];
     }
 }
