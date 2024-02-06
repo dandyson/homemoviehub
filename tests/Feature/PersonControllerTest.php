b42e47f0b39077b31b100cc862e34d559f9f0a0b
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2024-02-06 13:54:44 +0000

    More tests & title change from default

--- tests/Feature/PersonControllerTest.php
+++ tests/Feature/PersonControllerTest.php
@@ -54,5 +54,85 @@ class PersonControllerTest extends TestCase
         ); 
     }
 
-    // Add similar tests for other methods (create, store, show, edit, update, destroy)
+    /** @test */
+    public function create_method_displays_create_form()
+    {
+        $user = User::factory()->create();
+
+        $families = Family::factory()->count(3)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->actingAs($user)->get(route('person.create'));
+
+        $response->assertInertia(fn (Assert $page) => $page
+            ->component('Person/PersonDetails')
+            ->has('families', count($families), fn (Assert $page) => $page 
+                ->has('id')
+                ->has('name')
+                ->missing('created_at')
+                ->missing('updated_at')
+                ->etc()
+            )
+        ); 
+    }
+
+    /** @test */
+    public function store_method_creates_new_person_with_existing_family()
+    {
+        $user = User::factory()->create();
+        $family = Family::factory()->create([
+            'name' => 'Doe',
+            'user_id' => $user->id
+        ]);
+
+        $data = [
+            'name' => 'John',
+            'family' => ['name' => $family->name],
+        ];
+
+        $response = $this->actingAs($user)->post(route('person.store'), $data);
+
+        $response->assertStatus(200)
+            ->assertJson(['message' => ['type' => 'Success', 'text' => 'Person created successfully']]);
+        
+        $this->assertDatabaseHas('people', [
+            'name' => 'John',
+            'user_id' => $user->id,
+            'family_id' => $family->id,
+        ]);
+    }
+
+    /** @test */
+    public function store_method_creates_new_person_with_new_family()
+    {
+        $user = User::factory()->create();
+        $family = Family::factory()->create([
+            'name' => 'Doe',
+            'user_id' => $user->id
+        ]);
+
+        $data = [
+            'name' => 'John',
+            'family' => ['name' => 'Johnson'],
+        ];
+
+        $response = $this->actingAs($user)->post(route('person.store'), $data);
+
+        $response->assertStatus(200)
+            ->assertJson(['message' => ['type' => 'Success', 'text' => 'Person created successfully']]);
+
+        $newFamily = Family::where('name', 'Johnson')->first();
+        
+        $this->assertDatabaseHas('people', [
+            'name' => 'John',
+            'user_id' => Auth::id(),
+            'family_id' => $newFamily->id,
+        ]);
+
+        $this->assertDatabaseHas('families', [
+            'name' => 'Johnson',
+            'user_id' => $user->id,
+        ]);
+    }
 }
